bebd86c4bd1ad150fa1b1c3b6fee9173893985bf
Matt Puchlerz <matt@puchlerz.com>
2009-11-01 19:24:31 -0600

    Got some generic adapter specs passing

--- lib/dm-meetup/adapter.rb
+++ lib/dm-meetup/adapter.rb
@@ -1,12 +1,21 @@
 require 'dm-core/adapters/abstract_adapter'
+require 'open-uri'
 
 module DataMapper
   module Adapters
     class MeetupAdapter < AbstractAdapter
+      
+      attr_reader :api_key
 
-      # def initialize(name, options)
-      #   super
-      # end
+      def initialize name, options
+        super
+        @api_key = options[:api_key] if options[:api_key]
+      end
+      
+      def read query
+        uri = "http://api.meetup.com/"
+        open uri
+      end
 
     end
   end
--- spec/dm-meetup/adapter_spec.rb
+++ spec/dm-meetup/adapter_spec.rb
@@ -1,12 +1,33 @@
 require File.dirname(__FILE__) + '/../spec_helper'
-require 'dm-core/spec/adapter_shared_spec'
+# require 'dm-core/spec/adapter_shared_spec'
 
 describe DataMapper::Adapters::MeetupAdapter do
   
   before :all do
-    @adapter = DataMapper.setup :default, :adapter => 'meetup'
+    @adapter = DataMapper.setup :default, :adapter => 'meetup',
+                                          :api_key => 'HereIsMyApiKey1234567890'
+  end
+  
+  it "should read a Meetup API key" do
+    @adapter.api_key.should == 'HereIsMyApiKey1234567890'
+  end
+  
+  it "should implement #read" do
+    lambda { 
+      @adapter.read 'query' 
+    }.should_not raise_error(NotImplementedError)
+  end
+  
+  it "should make a request when trying to read" do
+    OpenURI.stub!(:open_uri).and_return ''
+    OpenURI.should_receive(:open_uri)
+    @adapter.read 'query'
   end
 
-  it_should_behave_like 'An Adapter'
+  # it_should_behave_like 'An Adapter'
+  # 
+  # Note that we aren't utilizing all of the standard 
+  # adapter behaviors here because we only care about 
+  # reading for the time being.
 
 end
